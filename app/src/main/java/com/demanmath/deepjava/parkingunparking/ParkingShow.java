6ab95e320cd71ce6fc3cacd8b2a3df8b4b11337c
deman <demanlu@126.com>
2020-09-17 11:30:08 +0800

    parking case

--- app/src/main/java/com/demanmath/deepjava/parkingunparking/ParkingShow.java
+++ app/src/main/java/com/demanmath/deepjava/parkingunparking/ParkingShow.java
@@ -28,5 +28,28 @@ public class ParkingShow {
 //        }
         TraceLog.d(String.valueOf(sum));
     }
+
+    public void fun2(){
+        int aa = 321;
+        Integer a=1;
+        Integer b=2;
+        Integer c=3;
+        Integer d=3;
+        Integer e=321;
+        Integer f=321;
+        Long g=3L;
+        Boolean bool = (g.longValue() == a.intValue()+b.intValue());
+        Boolean bool2 = g.equals(a+b);
+
+        System.out.println(c==d);//true 缓存池-128～127
+        System.out.println(e==f);//false
+        System.out.println(c==(a+b));//true
+        System.out.println(c.equals(a+b));//true
+        System.out.println(g==(a+b));//true 算术运算,自动拆箱，比较的是数值
+        System.out.println(g.equals(a+b));//false equals，自动装箱，比较的是类，一个Long，一个Int，肯定不一样。
+        System.out.println(g.equals(3));//false equals，自动装箱，比较的是类，一个Long，一个Int，肯定不一样。
+        System.out.println(e==aa);
+    }
+
 }
 
